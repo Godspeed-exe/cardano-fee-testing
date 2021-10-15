8fd4041cc46e1d28e81a4e27a070bead7c0b0d48
Michiel B <michiel.bellen@gmail.com>
2021-10-15 19:22:42 +0200

    complete tx body

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from pathlib import Path
 import json
+import bech32
+import sys
 
+
+def convert_to_hex(address):
+    recipient = address.split("+")
+    dec_recipient = bech32.bech32_decode(recipient[0])
+    index_recipient = recipient[1]
+    recipient_hexlist = bech32.convertbits(dec_recipient[1], 5, 16)
+    hex_recipient = ''.join([f'{c:04x}' for c in recipient_hexlist])[0:-2].upper()
+
+    return [hex_recipient, int(index_recipient)]
 
 status = True
 type_of_transaction=0
@@ -53,11 +64,25 @@ match type_of_transaction:
 
         for el in inputs:
             exploded = el.upper().split('#')
+            exploded[1] = int(exploded[1])
             main_inputs.append(exploded)
         
-        txinnerbody = {0: main_inputs,1: recipients}
+        recipient = input("To which address do you want to send these funds? example: addr1qye5yk4hev8y4ts7kjg9vej80jvr3jcny90kvvsfutr4vy70cnldrp0e0l8zkk0cdar60k5m5gnev2tk22q89ffz0ucsjv3a34+0")
+        change = input("To which address should the change be sent? example: addr1qygq56zcjm30nd7stemhxt0spgv8pv2gjzvxct86nksm7zr3thmphsj959ukugkm8rlh0phhkxh0h566ehagqy98nzhslwlep0+0")
+        
 
-        T = 0
+        recipients.append(convert_to_hex(recipient))
+        recipients.append(convert_to_hex(change))
+
+        txbody = []
+        txinnerbody = {0: main_inputs,1: recipients, 2: 0, 3: 0}
+        txbody.append(txinnerbody)
+        txbody.append([])
+        txbody.append(None)
+
+        size = sys.getsizeof(txbody)
+        print("size: {}".format(size))
+        T = size
         C = 0
         M = 0
     case _:
@@ -65,7 +90,7 @@ match type_of_transaction:
         C = 0
         M = 0
         
-print(txinnerbody)
+print(txbody)
 a = protocol_parameters['txFeePerByte']
 b = protocol_parameters['executionUnitPrices']['priceSteps']
 c = protocol_parameters['executionUnitPrices']['priceMemory']
